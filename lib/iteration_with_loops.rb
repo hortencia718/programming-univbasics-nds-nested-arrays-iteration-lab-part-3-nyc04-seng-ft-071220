61d8db075b79e60b4ccbbf88ea8a3904296b4b2f
hortencia c <hortenciaecisneros@gmail.com>
2020-07-25 01:53:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ def join_nested_strings(src)
  new_string = ""
   count = 0  # <-- index limit the loop and access the index
   while count < src.length do 
+    
     element = 0 
     inner_array = src[count]
     while element < inner_array.length do 
