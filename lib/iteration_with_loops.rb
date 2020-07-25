bf185d4e74820a0cba06688fbdd38d70441bfccf
hortencia c <hortenciaecisneros@gmail.com>
2020-07-25 00:57:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
     while element < inner_array.length do 
     inner_element=  inner_array[element]
     new_string += inner_element
-    if inner_element.is_a?(string)
+    if inner_element.is_a?(String)
     
     end
       element += 1
