275327200d4499d377667ea3757c986ef227a458
hortencia c <hortenciaecisneros@gmail.com>
2020-07-25 02:06:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 # require 'pry'
 def join_nested_strings(src)
-  #binding.pry
+  # binding.pry
  new_string = ""
   count = 0  # <-- index limit the loop and access the index
   while count < src.length do 
